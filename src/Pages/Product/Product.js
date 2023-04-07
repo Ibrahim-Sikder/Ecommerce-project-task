6e8343656e8ee80f2e42e4540bb1792c375462ef
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2023-04-08 03:41:36 +0600

    Ecommerce-project-setup-completed

---
+++ src/Pages/Product/Product.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import product from '../../Asset/product-img-2.10.jpg';
+import { FaHeart } from "react-icons/fa";
+import './Product.css';
+const Product = () => {
+    return (
+        
+        <div className='w-11/12 mx-auto my-10'>
+            <h2 className='text-3xl bold mb-5 font-bold'>Featured Products </h2>
+           <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-10">
+           <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+            <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+            <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+            <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+            <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+            <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+            <div className="productContainer w-[360px] h-[440px] text-center bg-white  relative">
+                <div className="ProductCard flex justify-center items-center">
+                    <img src={product} alt="" />
+                </div>
+                <div className="productText absolute bottom-6 left-[45px]">
+                <h2 className='text-2xl bold '>Callflex dolore eiusmod</h2>
+                <p className='text-xl'><span className='text-red-400'>BDT18.34</span> <del>BDT34.55</del></p>
+                
+            </div>
+            <div className=" productBtn absolute bottom-10 left-[90px]">
+                    <button className='px-8 py-3 bg-[#2D6F6D] text-white text-xl'>Add to Cart</button>
+                </div>
+                <div className='absolute duration-300  ease-in productIcon right-8 top-24 z-10'>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                    <FaHeart className='text-4xl bg-[#CCECEB] text-white p-2 mb-2'></FaHeart>
+                </div>
+            </div>
+           </div>
+          
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import hero from '../Asset/phone3.png'
+const Hero = () => {
+    return (
+        <div className='hero-area grid lg:grid-cols-2 items-center bg-[#0E0F20] text-white px-10 py-24'>
+            
+            <div className=''>
+               <h2 className='animate__rollIn animate__animated  animate__delay-.5s text-5xl font-bold font-fugiat mb-5 uppercase italic '>Welcome to my shop</h2>
+                <p className='animate__animated animate__backInLeft animate__delay-.5s'>Mobile phones became a very important element of well-developed societies. It is not only a device, but also a “smart” part of our life. Very often this devices support people with the answers to the various questions, like: opening hours, weather forecast, news, sport games results, clues, etc. So in a way, that devices solves everyday lifes issues and generally provides relief to peoples brains,</p>
+                <button className=' animate__animated animate__backInLeft animate__delay-.5s btn btn-outline bg-[#0E0F20] text-white px-8 mt-3 mb-5'>Buy Now </button>
+            </div>
+
+            <div className= 'animate__animated animate__delay-.5s animate__backInRight'>
+                <img className='w-full' src={hero} alt="" />
+            </div>
+
+
+           
+        </div>
+    );
+};
+
+export default Hero;
